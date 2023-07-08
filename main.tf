d42c963d7241e497e90b72542d851463a509d69f
sinhprab <sinha.pranshu2509@gmail.com>
2023-07-08 16:17:41 +0100

    added newline and removed extra space

--- main.tf
+++ main.tf
@@ -122,23 +122,19 @@ resource "aws_route_table" "private02" {
 resource "aws_route_table_association" "public01" {
   subnet_id      = aws_subnet.public01.id
   route_table_id = aws_route_table.public.id
-
 }
 
 resource "aws_route_table_association" "public02" {
   subnet_id      = aws_subnet.public02.id
   route_table_id = aws_route_table.public.id
-
 }
 
 resource "aws_route_table_association" "private01" {
   subnet_id      = aws_subnet.private01.id
   route_table_id = aws_route_table.private01.id
-
 }
 
 resource "aws_route_table_association" "private02" {
   subnet_id      = aws_subnet.private02.id
   route_table_id = aws_route_table.private02.id
-
 }
